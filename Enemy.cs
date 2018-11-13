1572e9d87e547a140f8abc0c1b852507e2dfb6ee
Str <strahinja.jkc@gmail.com>
2018-11-13 13:07:48 -0800

    testtest

---
+++ Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] List<Transform> positions;
+    int pos = 0;
+    float speed = 2f;
+    // Use this for initialization
+    void Start () {
+        
+        transform.position = positions[pos].transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (pos <= positions.Count)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, positions[pos].transform.position, speed * Time.deltaTime);
+            if (transform.position == positions[pos].transform.position)
+            {
+                pos++;
+            }
+        }
+        else {
+            Destroy(gameObject);
+        }
+		
+	}
+}
